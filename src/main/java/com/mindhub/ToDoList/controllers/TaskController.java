a9bd0acb54fae17bc8bc178b08787b2ec003154d
Mauricio <mauri.f.mores@gmail.com>
2024-09-16 17:06:21 -0300

    REFACTOR: add description

--- src/main/java/com/mindhub/ToDoList/controllers/TaskController.java
+++ src/main/java/com/mindhub/ToDoList/controllers/TaskController.java
@@ -1,10 +1,17 @@
 package com.mindhub.ToDoList.controllers;
 
 import com.mindhub.ToDoList.DTOs.NewTaskDTO;
+import com.mindhub.ToDoList.DTOs.TaskDTO;
 import com.mindhub.ToDoList.models.TaskStatus;
 import com.mindhub.ToDoList.services.ServiceTask;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("/api/task")
@@ -17,21 +24,151 @@ public class TaskController {
     }
 
     @PostMapping("/new_task")
+    @Operation(
+            summary = "Create a new task",
+            description = "This endpoint allows for the creation of a new task for a specified user. It accepts a JSON payload with the task details and returns the status of the operation.",
+            requestBody = @RequestBody(
+                    description = "JSON payload containing the task details",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(
+                                    implementation = NewTaskDTO.class,
+                                    example = "{ \"userId\": 123, \"title\": \"New Task Title\", \"description\": \"Detailed description of the task\" }"
+                            )
+                    )
+            ),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            description = "New task successfully created",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = String.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "206",
+                            description = "Partial content - title or description was not entered",
+                            content = @Content
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "User not found",
+                            content = @Content
+                    )
+            }
+    )
     public ResponseEntity<Object> newTask (@RequestBody NewTaskDTO newTaskDTO){
         return serviceTask.newTask(newTaskDTO);
     }
 
     @GetMapping("/get_task")
+    @Operation(
+            summary = "Retrieve task details",
+            description = "This endpoint retrieves the details of a task based on the provided task ID. It returns the task's information if the ID is valid and the task is active.",
+            parameters = {
+                    @Parameter(
+                            name = "id",
+                            description = "The unique identifier of the task",
+                            required = true,
+                            example = "456"
+                    )
+            },
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Task details retrieved successfully",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = TaskDTO.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Task not found",
+                            content = @Content
+                    )
+            }
+    )
     public ResponseEntity<Object> getTask (@RequestParam long id){
         return serviceTask.getTask(id);
     }
 
     @GetMapping("get_tasks")
+    @Operation(
+            summary = "Retrieve all active tasks",
+            description = "This endpoint retrieves a list of all active tasks in the system. It returns the task details if there are active tasks available.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "List of active tasks retrieved successfully",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = TaskDTO.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "204",
+                            description = "No active tasks found",
+                            content = @Content
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "Internal server error",
+                            content = @Content
+                    )
+            }
+    )
     public ResponseEntity<Object> getTasks (){
         return serviceTask.getTasks();
     }
 
     @PatchMapping("/patch_task")
+    @Operation(
+            summary = "Update task details",
+            description = "This endpoint allows for partial updates to a task's details. You can update the task's title, description, and status by providing the respective parameters.",
+            parameters = {
+                    @Parameter(
+                            name = "id",
+                            description = "The unique identifier of the task",
+                            required = true,
+                            example = "456"
+                    ),
+                    @Parameter(
+                            name = "title",
+                            description = "The new title of the task",
+                            required = false,
+                            example = "Updated Task Title"
+                    ),
+                    @Parameter(
+                            name = "description",
+                            description = "The new description of the task",
+                            required = false,
+                            example = "Updated detailed description of the task"
+                    ),
+                    @Parameter(
+                            name = "status",
+                            description = "The new status of the task",
+                            required = false,
+                            schema = @Schema(implementation = TaskStatus.class)
+                    )
+            },
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Task details updated successfully",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = String.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Task not found",
+                            content = @Content
+                    )
+            }
+    )
     public ResponseEntity<Object> patchTask (@RequestParam(required = true) long id,
                                              @RequestParam(required = false) String title,
                                              @RequestParam(required = false) String description,
@@ -40,6 +177,33 @@ public class TaskController {
     }
 
     @DeleteMapping("/delete_task")
+    @Operation(
+            summary = "Deactivate a task",
+            description = "This endpoint deactivates a task by setting its active status to false. It requires the task's unique identifier (ID) and returns the status of the operation.",
+            parameters = {
+                    @Parameter(
+                            name = "id",
+                            description = "The unique identifier of the task to be deactivated",
+                            required = true,
+                            example = "456"
+                    )
+            },
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Task successfully deactivated",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = String.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Task not found",
+                            content = @Content
+                    )
+            }
+    )
     public ResponseEntity<Object> deleteTask (@RequestParam(required = true) long id){
         return serviceTask.deleteTask(id);
     }
--- src/main/java/com/mindhub/ToDoList/controllers/UserController.java
+++ src/main/java/com/mindhub/ToDoList/controllers/UserController.java
@@ -1,9 +1,16 @@
 package com.mindhub.ToDoList.controllers;
 
 import com.mindhub.ToDoList.DTOs.NewUserDTO;
+import com.mindhub.ToDoList.DTOs.UserDTO;
 import com.mindhub.ToDoList.services.ServiceUser;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("/api/user")
@@ -16,21 +23,146 @@ public class UserController {
     }
 
     @PostMapping("/new_user")
+    @Operation(
+            summary = "Create a new user",
+            description = "Endpoint for user creation",
+            requestBody = @RequestBody(
+                    description = "JSON payload containing the user's details",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(
+                                    implementation = NewUserDTO.class,
+                                    example = "{ \"username\": \"john_doe\", \"password\": \"securePass123\", \"email\": \"john.doe@example.com\" }"
+                            )
+                    )
+            ),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            description = "New user created",
+                            content = { @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = String.class)) }),
+                    @ApiResponse(
+                            responseCode = "206",
+                            description = "Partial content",
+                            content = @Content),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Invalid password",
+                            content = @Content)
+            })
     public ResponseEntity<Object> newUser (@RequestBody NewUserDTO user){
         return serviceUser.newUser(user);
     }
 
     @GetMapping("/get_user")
+    @Operation(
+            summary = "Retrieve user details",
+            description = "This endpoint retrieves the details of a user based on the provided user ID. It returns the user's information if the ID is valid.",
+            parameters = {
+                    @Parameter(
+                            name = "id",
+                            description = "The unique identifier of the user",
+                            required = true,
+                            example = "123"
+                    )
+            },
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "User details retrieved successfully",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = UserDTO.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "User not found",
+                            content = @Content
+                    )
+            }
+    )
     public ResponseEntity<Object> getUser (@RequestParam long id){
         return serviceUser.getUser(id);
     }
 
     @GetMapping("/get_users")
+    @Operation(
+            summary = "Retrieve all active users",
+            description = "This endpoint retrieves a list of all active users in the system. It returns the user details if there are active users available.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "List of active users retrieved successfully",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = UserDTO.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "204",
+                            description = "No active users found",
+                            content = @Content
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "Internal server error",
+                            content = @Content
+                    )
+            }
+    )
     public ResponseEntity<Object> getUsers (){
         return serviceUser.getUsers();
     }
 
     @PatchMapping("/patch_user")
+    @Operation(
+            summary = "Update user details",
+            description = "This endpoint allows for partial updates to a user's details. You can update the user's email, password, and username by providing the respective parameters.",
+            parameters = {
+                    @Parameter(
+                            name = "id",
+                            description = "The unique identifier of the user",
+                            required = true,
+                            example = "123"
+                    ),
+                    @Parameter(
+                            name = "email",
+                            description = "The new email address of the user",
+                            required = false,
+                            example = "new.email@example.com"
+                    ),
+                    @Parameter(
+                            name = "password",
+                            description = "The new password for the user. It must include a capital letter, a number, or a special character, and be greater than six characters.",
+                            required = false,
+                            example = "NewPass123!"
+                    ),
+                    @Parameter(
+                            name = "userName",
+                            description = "The new username for the user",
+                            required = false,
+                            example = "new_username"
+                    )
+            },
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "User details updated successfully",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = String.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "User not found",
+                            content = @Content
+                    )
+            }
+    )
     public ResponseEntity<Object> patchUser(@RequestParam(required = true) Long id,
                                             @RequestParam(required = false) String email,
                                             @RequestParam(required = false) String password,
@@ -39,6 +171,33 @@ public class UserController {
     }
 
     @DeleteMapping("/delete_user")
+    @Operation(
+            summary = "Deactivate a user",
+            description = "This endpoint deactivates a user by setting their active status to false. It requires the user's unique identifier (ID) and returns the status of the operation.",
+            parameters = {
+                    @Parameter(
+                            name = "id",
+                            description = "The unique identifier of the user to be deactivated",
+                            required = true,
+                            example = "123"
+                    )
+            },
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "User successfully deactivated",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = String.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "User not found",
+                            content = @Content
+                    )
+            }
+    )
     public ResponseEntity<Object> deleteUser (@RequestParam(required = true) Long id){
         return serviceUser.deleteUser(id);
     }
