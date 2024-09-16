27fc44f1da17bd3566b79d47638af29cb43e6b27
Mauricio <mauri.f.mores@gmail.com>
2024-09-15 23:49:20 -0300

    REFACTOR: add test object instance

--- src/main/java/com/mindhub/ToDoList/ToDoListApplication.java
+++ src/main/java/com/mindhub/ToDoList/ToDoListApplication.java
@@ -1,7 +1,13 @@
 package com.mindhub.ToDoList;
 
+import com.mindhub.ToDoList.models.Task;
+import com.mindhub.ToDoList.models.UserEntity;
+import com.mindhub.ToDoList.repositories.RepositoryTask;
+import com.mindhub.ToDoList.repositories.RepositoryUser;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ToDoListApplication {
@@ -10,4 +16,22 @@ public class ToDoListApplication {
 		SpringApplication.run(ToDoListApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner initDataBase (RepositoryTask repositoryTask,
+										   RepositoryUser repositoryUser){
+		return (args) -> {
+
+			//new user object and task
+			UserEntity userOne = new UserEntity("Mauri", "1313Mauricio", "mauri@gmail.com");
+			Task taskOne = new Task("Shopping list", "Buy food for the dog");
+			repositoryUser.save(userOne);
+
+			//add task a user
+			userOne.addTask(taskOne);
+
+			//save in database
+			repositoryTask.save(taskOne);
+			repositoryUser.save(userOne);
+		};
+	}
 }
